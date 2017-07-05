510bc0dd899d6b5bf76cc76b9ab306b896a129be
HeerKirov <HeerKirov@outlook.com>
2017-07-05 17:01:43 +0800

    Change database to Postgresql,Add gunicorn and whitenoise

--- AttendanceSystem/settings.py
+++ AttendanceSystem/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'gunicorn',
+    'whitenoise',
     'main',
 ]
 
@@ -86,10 +88,20 @@ WSGI_APPLICATION = 'AttendanceSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'attendancesystem',
+        'USER': 'heer',
+        'PASSWORD': '262144',
+        'HOST': '120.25.229.109',
+        'PORT': '',
     }
 }
 
